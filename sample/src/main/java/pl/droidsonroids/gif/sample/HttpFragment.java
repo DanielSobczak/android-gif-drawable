a559bf91c917224bb878f548dafae46b83efa474
Daniel Sobczak <d.sobczak@allinmobile.co>
2017-08-04 08:03:52 +0200

    switch to GifImageView

--- sample/src/main/java/pl/droidsonroids/gif/sample/HttpFragment.java
+++ sample/src/main/java/pl/droidsonroids/gif/sample/HttpFragment.java
@@ -5,22 +5,24 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import pl.droidsonroids.gif.GifTextureView;
-import pl.droidsonroids.gif.InputSource;
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
 
 public class HttpFragment extends BaseFragment implements View.OnClickListener {
 
     private static final String KEY_ID = "key_id";
     private final ExecutorService mExecutorService = Executors.newSingleThreadExecutor();
-    private GifTextureView mGifTextureView;
+    private GifImageView mGifImageView;
 
     public static HttpFragment createInstance(int id) {
         HttpFragment httpFragment = new HttpFragment();
@@ -39,17 +41,17 @@ public class HttpFragment extends BaseFragment implements View.OnClickListener {
             }
             return null;
         } else {
-            mGifTextureView = (GifTextureView) inflater.inflate(R.layout.http, container, false);
+            mGifImageView = (GifImageView) inflater.inflate(R.layout.http, container, false);
             downloadGif();
-            return mGifTextureView;
+            return mGifImageView;
         }
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH && !mGifTextureView.isHardwareAccelerated()) {
-            Snackbar.make(mGifTextureView, R.string.gif_texture_view_stub_acceleration, Snackbar.LENGTH_LONG).show();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH && !mGifImageView.isHardwareAccelerated()) {
+            Snackbar.make(mGifImageView, R.string.gif_texture_view_stub_acceleration, Snackbar.LENGTH_LONG).show();
         }
     }
 
@@ -61,17 +63,21 @@ public class HttpFragment extends BaseFragment implements View.OnClickListener {
 
     @RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     void onGifDownloaded(ByteBuffer buffer) {
-        mGifTextureView.setInputSource(new InputSource.DirectByteBufferSource(buffer));
+        try {
+            mGifImageView.setImageDrawable(new GifDrawable(buffer));
+        } catch (IOException e) {
+            Log.e("GIF", "GifDrawable error: ", e);
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     void onDownloadFailed(Exception e) {
-        mGifTextureView.setOnClickListener(HttpFragment.this);
+        mGifImageView.setOnClickListener(HttpFragment.this);
         if (isDetached()) {
             return;
         }
         final String message = getString(R.string.gif_texture_view_loading_failed, e.getMessage());
-        Snackbar.make(mGifTextureView, message, Snackbar.LENGTH_LONG).show();
+        Snackbar.make(mGifImageView, message, Snackbar.LENGTH_LONG).show();
 
     }
 
